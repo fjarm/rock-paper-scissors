ecc02ad34eecf4827140db7069ae6d1c59358ce7
fjarm <129107554+fjarm@users.noreply.github.com>
2023-04-16 12:46:53 -0400

    feat: Add function for a game round

--- script.js
+++ script.js
@@ -8,17 +8,69 @@ function getComputerChoice()
     let computerChoice = getRandomInt(3)
     if (computerChoice === 0)
     {
-        return "Rock"
+        return "rock"
     }
     else if (computerChoice === 1)
     {
-        return "Paper"
+        return "paper"
     }
     else
     {
-        return "Scissors"
+        return "scissors"
     }
 
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection)
+{
+    playerSelection = playerSelection.toLowerCase()
+
+    switch(playerSelection)
+    {
+        case "rock":
+            if (computerSelection === "rock")
+            {
+                return "Tie!"
+            }
+            else if (computerSelection === "paper")
+            {
+                return "You Lose! Paper beats Rock."
+            }
+            else
+            {
+                return "You Win! Rock beats Scissors."
+            }
+        case "paper":
+            if (computerSelection === "rock")
+            {
+                return "You Win! Paper beats Rock."
+            }
+            else if (computerSelection === "paper")
+            {
+                return "Tie!"
+            }
+            else
+            {
+                return "You Lose! Scissors beats Paper."
+            }
+        case "scissors":
+            if (computerSelection === "rock")
+            {
+                return "You Lose! Rock beats Scissors."
+            }
+            else if (computerSelection === "paper")
+            {
+                return "You Win! Scissors beats Paper."
+            }
+            else
+            {
+                return "Tie!"
+            }
+        default:
+            return "Sorry, something went wrong."
+    }
+}
+
+const playerSelection = "rOck"
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
